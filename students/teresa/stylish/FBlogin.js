c06efb5d501e19916db7d546d8fdc2468b57c298
Teresa Hsieh <teresahsieh850506@gmail.com>
2019-07-29 21:59:08 +0800

    Try Button Function

--- students/teresa/stylish/FBlogin.js
+++ students/teresa/stylish/FBlogin.js
@@ -42,6 +42,7 @@ function testAPI() {
     console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function (response) {
         console.log('Successful login for: ' + response.name);
+        memberName = response.name;
         document.getElementById('status').innerHTML =
             'Thanks for logging in, ' + response.name + '!';
     });
